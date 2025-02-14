a4a5430fe8240a38e7a6c6b284ea29c5c541264b
shabgard1532 <oxiizhen1@gmail.com>
2025-02-14 18:42:36 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ dp = Dispatcher(bot)
 
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
-    await message.reply("سلام! ربات کراش‌یاب آماده است. :blush:")
+    await message.reply("سلام! ربات کراش‌یاب آماده است")
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
