6c273288914b65fc61487487fb08a806ffc6bf5f
shabgard1532 <oxiizhen1@gmail.com>
2025-02-14 20:43:34 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.utils import executor
+from aiogram.utils.executor import start_polling
 from flask import Flask
+import asyncio
 import os
 
-# توکن ربات رو اینجا بزار
+# توکن ربات
 API_TOKEN = "7746983847:AAHj7bO_3io6OyiZ-PsYMl0QxGStg-_3R6k"
 
 bot = Bot(token=API_TOKEN)
@@ -14,14 +15,18 @@ async def start_command(message: types.Message):
     await message.reply("سلام! ربات کراش‌یاب آماده است. :blush:")
 
 # سرور Flask برای Render
-app = Flask(__name__)
+app = Flask(name)
 
 @app.route('/')
 def home():
     return "Bot is running!"
 
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8080))
-    app.run(host="0.0.0.0", port=port)
+async def main():
+    """ اجرای همزمان Flask و aiogram """
+    loop = asyncio.get_event_loop()
+    loop.create_task(start_polling(dp, skip_updates=True))
 
-    executor.start_polling(dp, skip_updates=True)
+if name == "main":
+    port = int(os.environ.get("PORT", 10000))
+    asyncio.run(main())  # اجرای ربات
+    app.run(host="0.0.0.0", port=port)  # اجرای Flask
