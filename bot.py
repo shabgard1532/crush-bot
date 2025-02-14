7ac6a7359eba1bfbe37aa041570b113eca5d8bf1
shabgard1532 <oxiizhen1@gmail.com>
2025-02-14 17:57:38 +0330

    Add files via upload
    افزودن اولین نسخه ربات

---
+++ bot.py
@@ -0,0 +1,15 @@
+from aiogram import Bot, Dispatcher, types
+from aiogram.utils import executor
+
+# توکن ربات رو اینجا بزار
+API_TOKEN = "7746983847:AAHj7bO_3io6OyiZ-PsYMl0QxGStg-_3R6k"
+
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start_command(message: types.Message):
+    await message.reply("سلام! ربات کراش‌یاب آماده است. :blush:")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
