f1c52e0fc78f1bffe1c5e8ae1507c2a80972de31
shabgard1532 <oxiizhen1@gmail.com>
2025-02-15 01:41:03 +0330

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,22 +2,22 @@ import logging
 from aiogram import Bot, Dispatcher, types
 from aiogram.utils import executor
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.exceptions import TelegramAPIError
+from aiogram.exceptions import TelegramAPIError
 
 # تنظیمات لاگ‌گیری برای خطایابی
 logging.basicConfig(level=logging.INFO)
 
-# توکن ربات تلگرام
+# :small_blue_diamond: توکن ربات (حتماً مقدارش را تنظیم کن)
 API_TOKEN = "7746983847:AAHj7bO_3io6OyiZ-PsYMl0QxGStg-_3R6k"
 
-# شناسه کانال (با @ بنویس)
+# :small_blue_diamond: شناسه کانال (بدون @ ننویس)
 CHANNEL_ID = "@crushyab_bam"
 
 # مقداردهی ربات و دیسپچر
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-# تابع بررسی عضویت کاربر در کانال
+# :small_blue_diamond: تابع بررسی عضویت کاربر در کانال
 async def check_membership(user_id):
     try:
         member = await bot.get_chat_member(CHANNEL_ID, user_id)
@@ -25,41 +25,45 @@ async def check_membership(user_id):
     except TelegramAPIError:
         return False
 
-# هندلر /start
+# :small_blue_diamond: هندلر /start
 @dp.message_handler(commands=['start'])
 async def start_command(message: types.Message):
     user_id = message.from_user.id
-welcome_text = (":wave: به ربات کراش‌یاب بم خوش آمدید!\n\n
-  :small_blue_diamond: این ربات به شما کمک می‌کند تا کراش خود را پیدا کنید. اما قبل از شروع، ابتدا باید عضو کانال ما شوید. :white_check_mark:"
-member_text = ":white_check_mark: شما عضو کانال هستید، حالا می‌توانید از ربات استفاده کنید!")
-   
-await message.answer(welcome_text)
 
-    # بررسی عضویت کاربر
+    # :tada: پیام خوش‌آمدگویی
+    welcome_text = (
+        ":wave: به ربات کراش‌یاب بم خوش آمدید!\n\n"
+        ":cupid: در این ربات می‌توانید کراش خود را پیدا کنید.\n"
+        ":small_blue_diamond: اما قبل از شروع، لطفاً عضو کانال ما شوید. :white_check_mark:"
+    )
+    await message.answer(welcome_text)
+
+    # :small_blue_diamond: بررسی عضویت کاربر
     is_member = await check_membership(user_id)
     
     if is_member:
-        await message.answer(":white_check_mark: شما عضو کانال هستید، حالا می‌توانید از ربات استفاده کنید!")
+        await message.answer(":white_check_mark: شما عضو کانال هستید، حالا می‌توانید از ربات استفاده کنید! :speech_balloon:")
     else:
         keyboard = InlineKeyboardMarkup()
         keyboard.add(InlineKeyboardButton(":loudspeaker: عضویت در کانال", url=f"https://t.me/{CHANNEL_ID[1:]}"))
         keyboard.add(InlineKeyboardButton(":white_check_mark: عضو شدم", callback_data="check_membership"))
 
-        await message.answer(":warning: برای استفاده از ربات، ابتدا باید عضو کانال ما شوید.", reply_markup=keyboard)
+        await message.answer(":warning: برای استفاده از ربات، ابتدا باید عضو کانال ما شوید. :point_down:", reply_markup=keyboard)
 
-# بررسی دکمه "عضو شدم"
+# :small_blue_diamond: بررسی دکمه ":white_check_mark: عضو شدم"
 @dp.callback_query_handler(lambda c: c.data == "check_membership")
 async def verify_membership(callback_query: types.CallbackQuery):
     user_id = callback_query.from_user.id
     is_member = await check_membership(user_id)
     
     if is_member:
-        await bot.answer_callback_query(callback_query.id, ":white_check_mark: عضویت تأیید شد!")
-        await bot.send_message(user_id, ":white_check_mark: حالا می‌توانید از ربات استفاده کنید!")
+        await bot.answer_callback_query(callback_query.id, ":white_check_mark: عضویت شما تأیید شد! :tada:")
+        await bot.send_message(user_id, ":white_check_mark: حالا می‌توانید از ربات استفاده کنید! :speech_balloon:")
     else:
-        await bot.answer_callback_query(callback_query.id, ":x: هنوز عضو کانال نشده‌اید!")
+        await bot.answer_callback_query(callback_query.id, ":x: هنوز عضو کانال نشده‌اید! :name_badge:")
 
-# اجرای ربات
-if __name__ == "__main__":
+# :small_blue_diamond: اجرای ربات
+if name == "main":
     executor.start_polling(dp, skip_updates=True)
+
 
